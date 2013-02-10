django/django

commit 1cd6e04cd4f768bcd4385b75de433d497d938f82

Fixed #18676 -- Allow fast-path deletion of objects
Objects can be fast-path deleted if there are no signals, and there are
no further cascades. If fast-path is taken, the objects do not need to
be loaded into memory before deletion.

Thanks to Jeremy Dunck, Simon Charette and Alex Gaynor for reviewing
the patch.

django/db/models/query.py
@@ -529,6 +529,14 @@ class QuerySet(object):
         self._result_cache = None
     delete.alters_data = True
 
+    def _raw_delete(self, using):
+        """
+        Deletes objects found from the given queryset in single direct SQL
+        query. No signals are sent, and there is no protection for cascades.
+        """
+        sql.DeleteQuery(self.model).delete_qs(self, using)
+    _raw_delete.alters_data = True
+
     def update(self, **kwargs):
         """
         Updates all elements in the current QuerySet, setting all the given

tests/regressiontests/delete_regress/tests.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import datetime
 
 from django.conf import settings
-from django.db import backend, transaction, DEFAULT_DB_ALIAS
+from django.db import backend, transaction, DEFAULT_DB_ALIAS, models
 from django.test import TestCase, TransactionTestCase, skipUnlessDBFeature
 
 from .models import (Book, Award, AwardNote, Person, Child, Toy, PlayedWith,
@@ -139,15 +139,22 @@ class DeleteCascadeTransactionTests(TransactionTestCase):
         eaten = Eaten.objects.create(food=apple, meal="lunch")
 
         apple.delete()
+        self.assertFalse(Food.objects.exists())
+        self.assertFalse(Eaten.objects.exists())
+
 
 class LargeDeleteTests(TestCase):
     def test_large_deletes(self):
         "Regression for #13309 -- if the number of objects > chunk size, deletion still occurs"
         for x in range(300):
             track = Book.objects.create(pagecount=x+100)
+        # attach a signal to make sure we will not fast-delete
+        def noop(*args, **kwargs):
+            pass
+        models.signals.post_delete.connect(noop, sender=Book)
         Book.objects.all().delete()
+        models.signals.post_delete.disconnect(noop, sender=Book)
         self.assertEqual(Book.objects.count(), 0)
-
 
 
 class ProxyDeleteTest(TestCase):

tests/modeltests/delete/models.py
@@ -95,7 +95,7 @@ class MRNull(models.Model):
 
 
 class Avatar(models.Model):
-    pass
+    desc = models.TextField(null=True)
 
 
 class User(models.Model):
@@ -108,3 +108,21 @@ class HiddenUser(models.Model):
 
 class HiddenUserProfile(models.Model):
     user = models.ForeignKey(HiddenUser)
+
+class M2MTo(models.Model):
+    pass
+
+class M2MFrom(models.Model):
+    m2m = models.ManyToManyField(M2MTo)
+
+class Parent(models.Model):
+    pass
+
+class Child(Parent):
+    pass
+
+class Base(models.Model):
+    pass
+
+class RelToBase(models.Model):
+    base = models.ForeignKey(Base, on_delete=models.DO_NOTHING)

tests/modeltests/delete/tests.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 
-from django.db import models, IntegrityError
+from django.db import models, IntegrityError, connection
 from django.test import TestCase, skipUnlessDBFeature, skipIfDBFeature
 from django.utils.six.moves import xrange
 
 from .models import (R, RChild, S, T, U, A, M, MR, MRNull,
-    create_a, get_default_r, User, Avatar, HiddenUser, HiddenUserProfile)
+    create_a, get_default_r, User, Avatar, HiddenUser, HiddenUserProfile,
+    M2MTo, M2MFrom, Parent, Child, Base)
 
 
 class OnDeleteTests(TestCase):
@@ -73,6 +74,16 @@ class OnDeleteTests(TestCase):
         a = A.objects.get(pk=a.pk)
         self.assertEqual(replacement_r, a.donothing)
         models.signals.pre_delete.disconnect(check_do_nothing)
+
+    def test_do_nothing_qscount(self):
+        """
+        Test that a models.DO_NOTHING relation doesn't trigger a query.
+        """
+        b = Base.objects.create()
+        with self.assertNumQueries(1):
+            # RelToBase should not be queried.
+            b.delete()
+        self.assertEqual(Base.objects.count(), 0)
 
     def test_inheritance_cascade_up(self):
         child = RChild.objects.create()
@@ -229,16 +240,34 @@ class DeletionTests(TestCase):
         # 1 query to delete the avatar
         # The important thing is that when we can defer constraint checks there
         # is no need to do an UPDATE on User.avatar to null it out.
+
+        # Attach a signal to make sure we will not do fast_deletes.
+        calls = []
+        def noop(*args, **kwargs):
+            calls.append('')
+        models.signals.post_delete.connect(noop, sender=User)
+
         self.assertNumQueries(3, a.delete)
         self.assertFalse(User.objects.exists())
         self.assertFalse(Avatar.objects.exists())
+        self.assertEquals(len(calls), 1)
+        models.signals.post_delete.disconnect(noop, sender=User)
 
     @skipIfDBFeature("can_defer_constraint_checks")
     def test_cannot_defer_constraint_checks(self):
         u = User.objects.create(
             avatar=Avatar.objects.create()
         )
+        # Attach a signal to make sure we will not do fast_deletes.
+        calls = []
+        def noop(*args, **kwargs):
+            calls.append('')
+        models.signals.post_delete.connect(noop, sender=User)
+
         a = Avatar.objects.get(pk=u.avatar_id)
+        # The below doesn't make sense... Why do we need to null out
+        # user.avatar if we are going to delete the user immediately after it,
+        # and there are no more cascades.
         # 1 query to find the users for the avatar.
         # 1 query to delete the user
         # 1 query to null out user.avatar, because we can't defer the constraint
@@ -246,6 +275,8 @@ class DeletionTests(TestCase):
         self.assertNumQueries(4, a.delete)
         self.assertFalse(User.objects.exists())
         self.assertFalse(Avatar.objects.exists())
+        self.assertEquals(len(calls), 1)
+        models.signals.post_delete.disconnect(noop, sender=User)
 
     def test_hidden_related(self):
         r = R.objects.create()
@@ -254,3 +285,69 @@ class DeletionTests(TestCase):
 
         r.delete()
         self.assertEqual(HiddenUserProfile.objects.count(), 0)
+
+class FastDeleteTests(TestCase):
+
+    def test_fast_delete_fk(self):
+        u = User.objects.create(
+            avatar=Avatar.objects.create()
+        )
+        a = Avatar.objects.get(pk=u.avatar_id)
+        # 1 query to fast-delete the user
+        # 1 query to delete the avatar
+        self.assertNumQueries(2, a.delete)
+        self.assertFalse(User.objects.exists())
+        self.assertFalse(Avatar.objects.exists())
+
+    def test_fast_delete_m2m(self):
+        t = M2MTo.objects.create()
+        f = M2MFrom.objects.create()
+        f.m2m.add(t)
+        # 1 to delete f, 1 to fast-delete m2m for f
+        self.assertNumQueries(2, f.delete)
+
+    def test_fast_delete_revm2m(self):
+        t = M2MTo.objects.create()
+        f = M2MFrom.objects.create()
+        f.m2m.add(t)
+        # 1 to delete t, 1 to fast-delete t's m_set
+        self.assertNumQueries(2, f.delete)
+
+    def test_fast_delete_qs(self):
+        u1 = User.objects.create()
+        u2 = User.objects.create()
+        self.assertNumQueries(1, User.objects.filter(pk=u1.pk).delete)
+        self.assertEquals(User.objects.count(), 1)
+        self.assertTrue(User.objects.filter(pk=u2.pk).exists())
+
+    def test_fast_delete_joined_qs(self):
+        a = Avatar.objects.create(desc='a')
+        User.objects.create(avatar=a)
+        u2 = User.objects.create()
+        expected_queries = 1 if connection.features.update_can_self_select else 2
+        self.assertNumQueries(expected_queries,
+                              User.objects.filter(avatar__desc='a').delete)
+        self.assertEquals(User.objects.count(), 1)
+        self.assertTrue(User.objects.filter(pk=u2.pk).exists())
+
+    def test_fast_delete_inheritance(self):
+        c = Child.objects.create()
+        p = Parent.objects.create()
+        # 1 for self, 1 for parent
+        # However, this doesn't work as child.parent access creates a query,
+        # and this means we will be generating extra queries (a lot for large
+        # querysets). This is not a fast-delete problem.
+        # self.assertNumQueries(2, c.delete)
+        c.delete()
+        self.assertFalse(Child.objects.exists())
+        self.assertEquals(Parent.objects.count(), 1)
+        self.assertEquals(Parent.objects.filter(pk=p.pk).count(), 1)
+        # 1 for self delete, 1 for fast delete of empty "child" qs.
+        self.assertNumQueries(2, p.delete)
+        self.assertFalse(Parent.objects.exists())
+        # 1 for self delete, 1 for fast delete of empty "child" qs.
+        c = Child.objects.create()
+        p = c.parent_ptr
+        self.assertNumQueries(2, p.delete)
+        self.assertFalse(Parent.objects.exists())
+        self.assertFalse(Child.objects.exists())

tests/regressiontests/admin_util/models.py
@@ -39,3 +39,6 @@ class Guest(models.Model):
 
     class Meta:
         verbose_name = "awesome guest"
+
+class EventGuide(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.DO_NOTHING)

tests/regressiontests/admin_util/tests.py
@@ -17,7 +17,7 @@ from django.utils.formats import localize
 from django.utils.safestring import mark_safe
 from django.utils import six
 
-from .models import Article, Count, Event, Location
+from .models import Article, Count, Event, Location, EventGuide
 
 
 class NestedObjectsTests(TestCase):
@@ -70,6 +70,17 @@ class NestedObjectsTests(TestCase):
         # 1 query to fetch all children of 1 and 2 (none)
         # Should not require additional queries to populate the nested graph.
         self.assertNumQueries(2, self._collect, 0)
+
+    def test_on_delete_do_nothing(self):
+        """
+        Check that the nested collector doesn't query for DO_NOTHING objects.
+        """
+        n = NestedObjects(using=DEFAULT_DB_ALIAS)
+        objs = [Event.objects.create()]
+        EventGuide.objects.create(event=objs[0])
+        with self.assertNumQueries(2):
+            # One for Location, one for Guest, and no query for EventGuide
+            n.collect(objs)
 
 class UtilTests(unittest.TestCase):
     def test_values_from_lookup_field(self):

django/contrib/admin/util.py
@@ -191,6 +191,13 @@ class NestedObjects(Collector):
             roots.extend(self._nested(root, seen, format_callback))
         return roots
 
+    def can_fast_delete(self, *args, **kwargs):
+        """
+        We always want to load the objects into memory so that we can display
+        them to the user in confirm page.
+        """
+        return False
+
 
 def model_format_dict(obj):
     """

django/db/models/deletion.py
@@ -77,6 +77,9 @@ class Collector(object):
         self.data = {}
         self.batches = {} # {model: {field: set([instances])}}
         self.field_updates = {} # {model: {(field, value): set([instances])}}
+        # fast_deletes is a list of queryset-likes that can be deleted without
+        # fetching the objects into memory.
+        self.fast_deletes = [] 
 
         # Tracks deletion-order dependency for databases without transactions
         # or ability to defer constraint checks. Only concrete model classes
@@ -131,6 +134,43 @@ class Collector(object):
             model, {}).setdefault(
             (field, value), set()).update(objs)
 
+    def can_fast_delete(self, objs, from_field=None):
+        """
+        Determines if the objects in the given queryset-like can be
+        fast-deleted. This can be done if there are no cascades, no
+        parents and no signal listeners for the object class.
+
+        The 'from_field' tells where we are coming from - we need this to
+        determine if the objects are in fact to be deleted. Allows also
+        skipping parent -> child -> parent chain preventing fast delete of
+        the child.
+        """
+        if from_field and from_field.rel.on_delete is not CASCADE:
+            return False
+        if not (hasattr(objs, 'model') and hasattr(objs, '_raw_delete')):
+            return False
+        model = objs.model
+        if (signals.pre_delete.has_listeners(model)
+                or signals.post_delete.has_listeners(model)
+                or signals.m2m_changed.has_listeners(model)):
+            return False
+        # The use of from_field comes from the need to avoid cascade back to
+        # parent when parent delete is cascading to child.
+        opts = model._meta
+        if any(link != from_field for link in opts.concrete_model._meta.parents.values()):
+            return False
+        # Foreign keys pointing to this model, both from m2m and other
+        # models.
+        for related in opts.get_all_related_objects(
+            include_hidden=True, include_proxy_eq=True):
+            if related.field.rel.on_delete is not DO_NOTHING:
+                return False
+        # GFK deletes
+        for relation in opts.many_to_many:
+            if not relation.rel.through:
+                return False
+        return True
+
     def collect(self, objs, source=None, nullable=False, collect_related=True,
         source_attr=None, reverse_dependency=False):
         """
@@ -148,6 +188,9 @@ class Collector(object):
         models, the one case in which the cascade follows the forwards
         direction of an FK rather than the reverse direction.)
         """
+        if self.can_fast_delete(objs):
+            self.fast_deletes.append(objs)
+            return
         new_objs = self.add(objs, source, nullable,
                             reverse_dependency=reverse_dependency)
         if not new_objs:
@@ -160,6 +203,10 @@ class Collector(object):
         concrete_model = model._meta.concrete_model
         for ptr in six.itervalues(concrete_model._meta.parents):
             if ptr:
+                # FIXME: This seems to be buggy and execute a query for each
+                # parent object fetch. We have the parent data in the obj,
+                # but we don't have a nice way to turn that data into parent
+                # object instance.
                 parent_objs = [getattr(obj, ptr.name) for obj in new_objs]
                 self.collect(parent_objs, source=model,
                              source_attr=ptr.rel.related_name,
@@ -170,12 +217,12 @@ class Collector(object):
             for related in model._meta.get_all_related_objects(
                     include_hidden=True, include_proxy_eq=True):
                 field = related.field
-                if related.model._meta.auto_created:
-                    self.add_batch(related.model, field, new_objs)
-                else:
-                    sub_objs = self.related_objects(related, new_objs)
-                    if not sub_objs:
-                        continue
+                if field.rel.on_delete == DO_NOTHING:
+                    continue
+                sub_objs = self.related_objects(related, new_objs)
+                if self.can_fast_delete(sub_objs, from_field=field):
+                    self.fast_deletes.append(sub_objs)
+                elif sub_objs:
                     field.rel.on_delete(self, field, sub_objs, self.using)
 
             # TODO This entire block is only needed as a special case to
@@ -240,6 +287,10 @@ class Collector(object):
                 signals.pre_delete.send(
                     sender=model, instance=obj, using=self.using
                 )
+
+        # fast deletes
+        for qs in self.fast_deletes:
+            qs._raw_delete(using=self.using)
 
         # update fields
         for model, instances_for_fieldvalues in six.iteritems(self.field_updates):

R151327/Test-1

commit 082a5657bf358d212b5724bf435fa0982a0a0bd8

Fixed #19720 -- Oracle ordering related delete regression
When a query had a complex where condition (a condition targeting more
than the base table) a subquery was used for deletion. However, the
query had default ordering from the model's meta and Oracle doesn't
work with ordered subqueries.

The regression was caused by fast-path deletion code introduced in
1cd6e04cd4f768bcd4385b75de433d497d938f82 for fixing #18676.

Thanks to Dylan Klomparens for the report.

django/db/models/query.py
@@ -539,7 +539,7 @@ class QuerySet(object):
         # Disable non-supported fields.
         del_query.query.select_for_update = False
         del_query.query.select_related = False
-        del_query.query.clear_ordering()
+        del_query.query.clear_ordering(force_empty=True)
 
         collector = Collector(using=del_query.db)
         collector.collect(del_query)

tests/regressiontests/delete_regress/models.py
@@ -99,3 +99,13 @@ class OrgUnit(models.Model):
 class Login(models.Model):
     description = models.CharField(max_length=32)
     orgunit = models.ForeignKey(OrgUnit)
+
+class House(models.Model):
+    address = models.CharField(max_length=32)
+
+class OrderedPerson(models.Model):
+    name = models.CharField(max_length=32)
+    lives_in = models.ForeignKey(House)
+
+    class Meta:
+        ordering = ['name']

tests/regressiontests/delete_regress/tests.py
@@ -10,7 +10,7 @@ from django.test import TestCase, TransactionTestCase, skipUnlessDBFeature
 from .models import (Book, Award, AwardNote, Person, Child, Toy, PlayedWith,
     PlayedWithNote, Email, Researcher, Food, Eaten, Policy, Version, Location,
     Item, Image, File, Photo, FooFile, FooImage, FooPhoto, FooFileProxy, Login,
-    OrgUnit)
+    OrgUnit, OrderedPerson, House)
 
 
 # Can't run this test under SQLite, because you can't
@@ -347,3 +347,14 @@ class Ticket19102Tests(TestCase):
         self.assertFalse(Login.objects.filter(pk=self.l1.pk).exists())
         self.assertTrue(Login.objects.filter(pk=self.l2.pk).exists())
 
+
+class OrderedDeleteTests(TestCase):
+    def test_meta_ordered_delete(self):
+        # When a subquery is performed by deletion code, the subquery must be
+        # cleared of all ordering. There was a but that caused _meta ordering
+        # to be used. Refs #19720.
+        h = House.objects.create(address='Foo')
+        OrderedPerson.objects.create(name='Jack', lives_in=h)
+        OrderedPerson.objects.create(name='Bob', lives_in=h)
+        OrderedPerson.objects.filter(lives_in__address='Foo').delete()
+        self.assertEqual(OrderedPerson.objects.count(), 0)