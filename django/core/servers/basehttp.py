django/django

Fixed #21773 -- made daemon threads default in the development server.
Also added an option to use non-daemon threads in the runserver command.
The new option is documented in the docs.
Please read my comment on the code to better understand the usefulness
of changing the default.
Thanks clime for the report.

django/core/servers/basehttp.py
@@ -156,12 +156,20 @@ class WSGIRequestHandler(simple_server.WSGIRequestHandler, object):
         sys.stderr.write(msg)
 
 
-def run(addr, port, wsgi_handler, ipv6=False, threading=False):
+def run(addr, port, wsgi_handler, ipv6=False, threading=False, daemon_threads=False):
     server_address = (addr, port)
     if threading:
         httpd_cls = type(str('WSGIServer'), (socketserver.ThreadingMixIn, WSGIServer), {})
     else:
         httpd_cls = WSGIServer
     httpd = httpd_cls(server_address, WSGIRequestHandler, ipv6=ipv6)
+    if threading:
+        # ThreadingMixIn.daemon_threads indicates how threads will behave on an
+        # abrupt shutdown; like quitting the server by the user or restarting
+        # by the auto-reloader. True means the server will not wait for thread
+        # termination before it quits. This will make auto-reloader faster
+        # and will prevent the need to kill the server manually if a thread
+        # isn't terminating correctly.
+        httpd.daemon_threads = daemon_threads
     httpd.set_app(wsgi_handler)
     httpd.serve_forever()

R151327/Test-1

django/core/servers/basehttp.py
@@ -115,5 +115,13 @@ def run(addr, port, wsgi_handler, ipv6=False, threading=False):
     else:
         httpd_cls = WSGIServer
     httpd = httpd_cls(server_address, WSGIRequestHandler, ipv6=ipv6)
+    if threading:
+        # ThreadingMixIn.daemon_threads indicates how threads will behave on an
+        # abrupt shutdown; like quitting the server by the user or restarting
+        # by the auto-reloader. True means the server will not wait for thread
+        # termination before it quits. This will make auto-reloader faster
+        # and will prevent the need to kill the server manually if a thread
+        # isn't terminating correctly.
+        httpd.daemon_threads = True
     httpd.set_app(wsgi_handler)
     httpd.serve_forever()