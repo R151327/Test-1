django/django

commit b9f8635f58ad743995cad2081b3dc395e55761e5

Refs #16508 -- Added invalidation of stale cached instances of GenericForeignKey targets.

tests/generic_relations/tests.py
@@ -555,6 +555,23 @@ id="id_generic_relations-taggeditem-content_type-object_id-1-id" /></p>""" % tag
         with self.assertRaises(IntegrityError):
             TaggedItem.objects.create(tag="shiny", content_object=quartz)
 
+    def test_cache_invalidation_for_content_type_id(self):
+        # Create a Vegetable and Mineral with the same id.
+        new_id = max(Vegetable.objects.order_by('-id')[0].id,
+                     Mineral.objects.order_by('-id')[0].id) + 1
+        broccoli = Vegetable.objects.create(id=new_id, name="Broccoli")
+        diamond = Mineral.objects.create(id=new_id, name="Diamond", hardness=7)
+        tag = TaggedItem.objects.create(content_object=broccoli, tag="yummy")
+        tag.content_type = ContentType.objects.get_for_model(diamond)
+        self.assertEqual(tag.content_object, diamond)
+
+    def test_cache_invalidation_for_object_id(self):
+        broccoli = Vegetable.objects.create(name="Broccoli")
+        cauliflower = Vegetable.objects.create(name="Cauliflower")
+        tag = TaggedItem.objects.create(content_object=broccoli, tag="yummy")
+        tag.object_id = cauliflower.id
+        self.assertEqual(tag.content_object, cauliflower)
+
 
 class CustomWidget(forms.TextInput):
     pass

django/contrib/contenttypes/fields.py
@@ -208,7 +208,7 @@ class GenericForeignKey(object):
                 lambda obj: (obj._get_pk_val(), obj.__class__),
                 gfk_key,
                 True,
-                self.cache_attr)
+                self.name)
 
     def is_cached(self, instance):
         return hasattr(instance, self.cache_attr)
@@ -217,25 +217,34 @@ class GenericForeignKey(object):
         if instance is None:
             return self
 
+        # Don't use getattr(instance, self.ct_field) here because that might
+        # reload the same ContentType over and over (#5570). Instead, get the
+        # content type ID here, and later when the actual instance is needed,
+        # use ContentType.objects.get_for_id(), which has a global cache.
+        f = self.model._meta.get_field(self.ct_field)
+        ct_id = getattr(instance, f.get_attname(), None)
+        pk_val = getattr(instance, self.fk_field)
+
         try:
-            return getattr(instance, self.cache_attr)
+            rel_obj = getattr(instance, self.cache_attr)
         except AttributeError:
             rel_obj = None
+        else:
+            if rel_obj and (ct_id != self.get_content_type(obj=rel_obj, using=instance._state.db).id or
+                            rel_obj._meta.pk.to_python(pk_val) != rel_obj._get_pk_val()):
+                rel_obj = None
 
-            # Make sure to use ContentType.objects.get_for_id() to ensure that
-            # lookups are cached (see ticket #5570). This takes more code than
-            # the naive ``getattr(instance, self.ct_field)``, but has better
-            # performance when dealing with GFKs in loops and such.
-            f = self.model._meta.get_field(self.ct_field)
-            ct_id = getattr(instance, f.get_attname(), None)
-            if ct_id is not None:
-                ct = self.get_content_type(id=ct_id, using=instance._state.db)
-                try:
-                    rel_obj = ct.get_object_for_this_type(pk=getattr(instance, self.fk_field))
-                except ObjectDoesNotExist:
-                    pass
-            setattr(instance, self.cache_attr, rel_obj)
+        if rel_obj is not None:
             return rel_obj
+
+        if ct_id is not None:
+            ct = self.get_content_type(id=ct_id, using=instance._state.db)
+            try:
+                rel_obj = ct.get_object_for_this_type(pk=pk_val)
+            except ObjectDoesNotExist:
+                pass
+        setattr(instance, self.cache_attr, rel_obj)
+        return rel_obj
 
     def __set__(self, instance, value):
         ct = None

R151327/Test-1

commit 043971cb0bee7462e818f1915332e3f7492ab06f

Fixed #28856 -- Fixed a regression in caching of a GenericForeignKey pointing to a MTI model.
Regression in b9f8635f58ad743995cad2081b3dc395e55761e5.

django/db/models/fields/related.py
@@ -872,6 +872,9 @@ class ForeignKey(ForeignObject):
             kwargs['to_field'] = self.remote_field.field_name
         return name, path, args, kwargs
 
+    def to_python(self, value):
+        return self.target_field.to_python(value)
+
     @property
     def target_field(self):
         return self.foreign_related_fields[0]

tests/generic_relations_regress/tests.py
@@ -48,6 +48,12 @@ class GenericRelationTests(TestCase):
         TextLink.objects.create(content_object=oddrel)
         oddrel.delete()
 
+    def test_coerce_object_id_remote_field_cache_persistence(self):
+        restaurant = Restaurant.objects.create()
+        CharLink.objects.create(content_object=restaurant)
+        charlink = CharLink.objects.latest('pk')
+        self.assertIs(charlink.content_object, charlink.content_object)
+
     def test_q_object_or(self):
         """
         SQL query parameters for generic relations are properly

tests/model_fields/test_foreignkey.py
@@ -93,3 +93,13 @@ class ForeignKeyTests(TestCase):
 
         assert_app_model_resolved('model_fields')
         assert_app_model_resolved('tests')
+
+    @isolate_apps('model_fields')
+    def test_to_python(self):
+        class Foo(models.Model):
+            pass
+
+        class Bar(models.Model):
+            fk = models.ForeignKey(Foo, models.CASCADE)
+
+        self.assertEqual(Bar._meta.get_field('fk').to_python('1'), 1)

tests/serializers/test_xml.py
@@ -30,12 +30,6 @@ class XmlSerializerTestCase(SerializersTestBase, TestCase):
 </django-objects>"""  # NOQA
 
     @staticmethod
-    def _comparison_value(value):
-        # The XML serializer handles everything as strings, so comparisons
-        # need to be performed on the stringified value
-        return str(value)
-
-    @staticmethod
     def _validate_output(serial_str):
         try:
             minidom.parseString(serial_str)

tests/serializers/tests.py
@@ -90,10 +90,6 @@ class SerializerRegistrationTests(SimpleTestCase):
 class SerializersTestBase:
     serializer_name = None  # Set by subclasses to the serialization format name
 
-    @staticmethod
-    def _comparison_value(value):
-        return value
-
     def setUp(self):
         sports = Category.objects.create(name="Sports")
         music = Category.objects.create(name="Music")
@@ -193,7 +189,7 @@ class SerializersTestBase:
         self.assertFalse(self._get_field_values(serial_str, 'author'))
 
         for obj in serializers.deserialize(self.serializer_name, serial_str):
-            self.assertEqual(obj.object.pk, self._comparison_value(self.joe.pk))
+            self.assertEqual(obj.object.pk, self.joe.pk)
 
     def test_serialize_field_subset(self):
         """Output can be restricted to a subset of fields"""