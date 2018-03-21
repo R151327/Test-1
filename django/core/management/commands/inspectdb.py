django/django

commit c76d87427d70f6c91ab855ed688828aa982720d2

Refs #23919 -- Removed obsolete comments about u'' prefixes.

django/core/management/commands/inspectdb.py
@@ -273,10 +273,7 @@ class Command(BaseCommand):
             if params['unique']:
                 columns = params['columns']
                 if len(columns) > 1:
-                    # we do not want to include the u"" or u'' prefix
-                    # so we build the string rather than interpolate the tuple
-                    tup = '(' + ', '.join("'%s'" % column_to_field_name[c] for c in columns) + ')'
-                    unique_together.append(tup)
+                    unique_together.append('(' + str(tuple(column_to_field_name[c] for c in columns)) + ')')
         managed_comment = "  # Created from a view. Don't remove." if is_view else ""
         meta = ["",
                 "    class Meta:",

tests/admin_changelist/tests.py
@@ -905,11 +905,8 @@ class AdminLogNodeTestCase(TestCase):
         """
         context = Context({'user': CustomIdUser()})
         template_string = '{% load log %}{% get_admin_log 10 as admin_log for_user user %}'
-
         template = Template(template_string)
-
-        # Rendering should be u'' since this templatetag just logs,
-        # it doesn't render any string.
+        # This template tag just logs.
         self.assertEqual(template.render(context), '')
 
     def test_get_admin_log_templatetag_no_user(self):

R151327/Test-1

commit 412745ddab895a12ba1948032c12e437a0b64a19

Fixed too many parentheses in inspectdb Meta.unique_together output.
Regression in c76d87427d70f6c91ab855ed688828aa982720d2.

django/core/management/commands/inspectdb.py
@@ -273,7 +273,7 @@ class Command(BaseCommand):
             if params['unique']:
                 columns = params['columns']
                 if len(columns) > 1:
-                    unique_together.append('(' + str(tuple(column_to_field_name[c] for c in columns)) + ')')
+                    unique_together.append(str(tuple(column_to_field_name[c] for c in columns)))
         managed_comment = "  # Created from a view. Don't remove." if is_view else ""
         meta = ["",
                 "    class Meta:",

tests/inspectdb/tests.py
@@ -211,6 +211,7 @@ class InspectDBTestCase(TestCase):
         out = StringIO()
         call_command('inspectdb', 'inspectdb_uniquetogether', stdout=out)
         output = out.getvalue()
+        self.assertIn("    unique_together = (('", output)
         unique_re = re.compile(r'.*unique_together = \((.+),\).*')
         unique_together_match = re.findall(unique_re, output)
         # There should be one unique_together tuple.