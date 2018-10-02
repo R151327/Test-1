django/django

Refs #29641 -- Extracted reusable CheckConstraint logic into a base class.

django/db/models/constraints.py
@@ -3,21 +3,12 @@ from django.db.models.sql.query import Query
 __all__ = ['CheckConstraint']
 
 
-class CheckConstraint:
-    def __init__(self, constraint, name):
-        self.constraint = constraint
+class BaseConstraint:
+    def __init__(self, name):
         self.name = name
 
     def constraint_sql(self, model, schema_editor):
-        query = Query(model)
-        where = query.build_where(self.constraint)
-        connection = schema_editor.connection
-        compiler = connection.ops.compiler('SQLCompiler')(query, connection, 'default')
-        sql, params = where.as_sql(compiler, connection)
-        params = tuple(schema_editor.quote_value(p) for p in params)
-        return schema_editor.sql_check_constraint % {
-            'check': sql % params,
-        }
+        raise NotImplementedError
 
     def create_sql(self, model, schema_editor):
         sql = self.constraint_sql(model, schema_editor)
@@ -34,6 +25,30 @@ class CheckConstraint:
             'name': quote_name(self.name),
         }
 
+    def deconstruct(self):
+        path = '%s.%s' % (self.__class__.__module__, self.__class__.__name__)
+        path = path.replace('django.db.models.constraints', 'django.db.models')
+        return (path, (), {'name': self.name})
+
+    def clone(self):
+        _, args, kwargs = self.deconstruct()
+        return self.__class__(*args, **kwargs)
+
+
+class CheckConstraint(BaseConstraint):
+    def __init__(self, constraint, name):
+        self.constraint = constraint
+        super().__init__(name)
+
+    def constraint_sql(self, model, schema_editor):
+        query = Query(model)
+        where = query.build_where(self.constraint)
+        connection = schema_editor.connection
+        compiler = connection.ops.compiler('SQLCompiler')(query, connection, 'default')
+        sql, params = where.as_sql(compiler, connection)
+        params = tuple(schema_editor.quote_value(p) for p in params)
+        return schema_editor.sql_check_constraint % {'check': sql % params}
+
     def __repr__(self):
         return "<%s: constraint='%s' name='%s'>" % (self.__class__.__name__, self.constraint, self.name)
 
@@ -45,10 +60,6 @@ class CheckConstraint:
         )
 
     def deconstruct(self):
-        path = '%s.%s' % (self.__class__.__module__, self.__class__.__name__)
-        path = path.replace('django.db.models.constraints', 'django.db.models')
-        return (path, (), {'constraint': self.constraint, 'name': self.name})
-
-    def clone(self):
-        _, args, kwargs = self.deconstruct()
-        return self.__class__(*args, **kwargs)
+        path, args, kwargs = super().deconstruct()
+        kwargs['constraint'] = self.constraint
+        return path, args, kwargs

R151327/Test-1

django/db/models/constraints.py
@@ -3,22 +3,12 @@ from django.db.models.sql.query import Query
 __all__ = ['CheckConstraint']
 
 
-class CheckConstraint:
-    def __init__(self, *, check, name):
-        self.check = check
+class BaseConstraint:
+    def __init__(self, name):
         self.name = name
 
     def constraint_sql(self, model, schema_editor):
-        query = Query(model)
-        where = query.build_where(self.check)
-        connection = schema_editor.connection
-        compiler = connection.ops.compiler('SQLCompiler')(query, connection, 'default')
-        sql, params = where.as_sql(compiler, connection)
-        params = tuple(schema_editor.quote_value(p) for p in params)
-        return schema_editor.sql_check % {
-            'name': schema_editor.quote_name(self.name),
-            'check': sql % params,
-        }
+        raise NotImplementedError('This method must be implemented by a subclass.')
 
     def create_sql(self, model, schema_editor):
         sql = self.constraint_sql(model, schema_editor)
@@ -34,6 +24,33 @@ class CheckConstraint:
             'name': quote_name(self.name),
         }
 
+    def deconstruct(self):
+        path = '%s.%s' % (self.__class__.__module__, self.__class__.__name__)
+        path = path.replace('django.db.models.constraints', 'django.db.models')
+        return (path, (), {'name': self.name})
+
+    def clone(self):
+        _, args, kwargs = self.deconstruct()
+        return self.__class__(*args, **kwargs)
+
+
+class CheckConstraint(BaseConstraint):
+    def __init__(self, *, check, name):
+        self.check = check
+        super().__init__(name)
+
+    def constraint_sql(self, model, schema_editor):
+        query = Query(model)
+        where = query.build_where(self.check)
+        connection = schema_editor.connection
+        compiler = connection.ops.compiler('SQLCompiler')(query, connection, 'default')
+        sql, params = where.as_sql(compiler, connection)
+        params = tuple(schema_editor.quote_value(p) for p in params)
+        return schema_editor.sql_check % {
+            'name': schema_editor.quote_name(self.name),
+            'check': sql % params,
+        }
+
     def __repr__(self):
         return "<%s: check='%s' name=%r>" % (self.__class__.__name__, self.check, self.name)
 
@@ -45,10 +62,6 @@ class CheckConstraint:
         )
 
     def deconstruct(self):
-        path = '%s.%s' % (self.__class__.__module__, self.__class__.__name__)
-        path = path.replace('django.db.models.constraints', 'django.db.models')
-        return (path, (), {'check': self.check, 'name': self.name})
-
-    def clone(self):
-        _, args, kwargs = self.deconstruct()
-        return self.__class__(*args, **kwargs)
+        path, args, kwargs = super().deconstruct()
+        kwargs['check'] = self.check
+        return path, args, kwargs